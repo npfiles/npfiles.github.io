6a0a829454407ac862794766a97bd671c9f24841
staff-netpartner <staff@netpartner.jp>
2018-06-26 14:13:35 +0900

    Update render.js

--- render.js
+++ render.js
@@ -2,7 +2,7 @@ var uri_enc = encodeURI(location.href);
 var data = "url=" + uri_enc;
 $.ajax({
   type: "POST",
-  url: "http://netpartner.sakura.ne.jp/comp/medium.php",
+  url: "//netpartner.sakura.ne.jp/comp/medium.php",
   cache: false,
   data: data,
   success: function(html){
